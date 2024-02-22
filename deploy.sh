4d4c674839e2d2f19644c6216876870be3f22801
DragonSSS <songweilong1989@gmail.com>
2024-02-21 22:33:36 -0500

    -fix

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,18 @@ set -e
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo $AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
 git config --global credential.helper store
 git config --global user.email "dragonsss-blog-bot@users.noreply.github.com"
 git config --global user.name "dragonsss-blog-bot"
 git config --global push.default simple
 
 rm -rf deployment
-git clone -b master https://github.com/DragonSSS/dragonsss.github.io.git deployment
+git clone -b master https://${GH_TOKEN}@github.com/DragonSSS/dragonsss.github.io.git deployment
 rsync -av --delete --exclude ".git" public/ deployment
 cd deployment
 git add -A
 git commit -m "rebuilding site on $(date), commit ${GITHUB_SHA} and GitHub Actions run number ${GITHUB_RUN_NUMBER}" || true
-git push
+git push https://${GH_TOKEN}@github.com/DragonSSS/dragonsss.github.io.git master
 
 cd ..
 rm -rf deployment
