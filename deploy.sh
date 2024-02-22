37cc3e0072d44b444ca239f5d45b40fb6ba947d6
DragonSSS <songweilong1989@gmail.com>
2024-02-21 23:12:22 -0500

    -fix

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ set -e
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+echo "Token length: ${#GH_TOKEN}"
+
 git config --global user.email "dragonsss-blog-bot@users.noreply.github.com"
 git config --global user.name "dragonsss-blog-bot"
-git config --global url.https://${GH_TOKEN}@github.com/.insteadOf https://github.com/
 
 rm -rf deployment
 git clone -b master https://${GH_TOKEN}@github.com/DragonSSS/dragonsss.github.io.git deployment
@@ -14,7 +15,8 @@ rsync -av --delete --exclude ".git" public/ deployment
 cd deployment
 git add -A
 git commit -m "rebuilding site on $(date), commit ${GITHUB_SHA} and GitHub Actions run number ${GITHUB_RUN_NUMBER}" || true
-git push https://${GH_TOKEN}@github.com/DragonSSS/dragonsss.github.io.git master
+git remote set-url master https://${GH_TOKEN}@github.com/DragonSSS/dragonsss.github.io.git
+git push
 
 cd ..
 rm -rf deployment
