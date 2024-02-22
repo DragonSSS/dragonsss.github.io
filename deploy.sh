2932f95b893992dad4882148b12a6fe963e38417
DragonSSS <songweilong1989@gmail.com>
2024-02-21 22:15:48 -0500

    github action

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo $GITHUB_AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
+echo $AUTH_SECRET > ~/.git-credentials && chmod 0600 ~/.git-credentials
 git config --global credential.helper store
 git config --global user.email "dragonsss-blog-bot@users.noreply.github.com"
 git config --global user.name "dragonsss-blog-bot"
